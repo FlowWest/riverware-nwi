9a2cdec2d41a3a74fa3f2d4d1c44eebe50664099
InigoP <ipeng@flowwest.com>
2025-03-18 10:05:51 -0700

    generate data summary

--- scripts/prism-download.R
+++ scripts/prism-download.R
@@ -5,7 +5,6 @@ library(stringr)
 library(tidyverse)
 element_list <- c("ppt", "tmean", "tmax")
 prism_base_url <- "http://services.nacse.org/prism/data/public/4km"
-# element <- "tmean"
 start_date <- as.Date("2025-02-01")
 stop_date <- as.Date("2025-02-03")
 prism_locations <- readRDS("data/prism_data/prism_locations")
@@ -19,15 +18,15 @@ for (element in element_list){
   for (current_date in seq(start_date, stop_date, by = "day")) {
     day <- format(as.Date(current_date), "%Y%m%d")
     download_url <- paste0(prism_base_url, "/", element, "/", day, "?format=nc")
-    download.file(download_url, destfile = paste0("data-raw/Prism_Files/",day,"_",element, ".zip"), mode = "wb")
+    download.file(download_url, destfile = paste0("data-raw/Prism_Files/",element,"/",day,"_",element, ".zip"), mode = "wb")
     Sys.sleep(2)
   }
-  nc_files <- list.files(path = "data-raw/Prism_Files", pattern = "*.zip", full.names = TRUE)
+  nc_files <- list.files(path = paste0("data-raw/Prism_Files/", element), pattern = "*.zip", full.names = TRUE)
 
   for (file in nc_files){
     date_str <- str_extract(file, "[0-9]{8}")
-    unzip(file, exdir = "data-raw/Prism_Files/unzipped/")
-    ncin <- nc_open(paste0("data-raw/Prism_Files/unzipped/PRISM_", element,"_provisional_4kmD2_", date_str, "_nc.nc"))
+    unzip(file, exdir = paste0("data-raw/Prism_Files/", element, "/"))
+    ncin <- nc_open(paste0("data-raw/Prism_Files/", element, "/PRISM_", element,"_provisional_4kmD2_", date_str, "_nc.nc"))
     lon <- ncvar_get(ncin,"lon")
     lat <- ncvar_get(ncin, "lat")
     element_val <-ncvar_get(ncin, "Band1")
@@ -57,29 +56,27 @@ for (element in element_list){
   }
 }
 
-date_col <- names(prism_ppt_data[-c(1:3)])
+# date_col <- names(prism_ppt_data[-c(1:3)])
 ppt_long_data <- prism_ppt_data |>
   dplyr::select(-c(longitude, latitude)) |>
   pivot_longer(!name, names_to = "date", values_to = "ppt")
-tmean_long_data <- tmean_prism_data |>
+tmean_long_data <- prism_tmean_data |>
   dplyr::select(-c(longitude, latitude)) |>
   pivot_longer(!name, names_to = "date", values_to = "tmean")
-tmax_long_data <- tmax_prism_data |>
+tmax_long_data <- prism_tmax_data |>
   dplyr::select(-c(longitude, latitude)) |>
   pivot_longer(!name, names_to = "date", values_to = "tmax")
 full_download_data <- ppt_long_data |>
   full_join(tmean_long_data, by = c("name", "date")) |>
-  full_join(tmax_long_data, by = c("name", "date"))
+  full_join(tmax_long_data, by = c("name", "date")) |>
+  mutate(across(where(is.numeric), ~ round(.x, 2)))
 write_csv(full_download_data, "data/prism_data/prism_download.csv")
-
-# wide_data <-  ppt_long_data |>
-#   pivot_wider(names_from = "name", values_from = "ppt")
 
 ppt_data_summary <- ppt_long_data |>
   mutate(group = case_when(
     startsWith(name, "UKL") ~ "UKL_ppt",
     startsWith(name, "WIL") ~ "Williamson_ppt",
-    startsWith(name, "SPR") ~ "Sprague_ppt "
+    startsWith(name, "SPR") ~ "Sprague_ppt"
   )) |>
   group_by(date, group) |>
   summarise(mean = mean(ppt)) |>
@@ -88,7 +85,7 @@ tmean_data_summary <- tmean_long_data |>
   mutate(group = case_when(
     startsWith(name, "UKL") ~ "UKL_tmean",
     startsWith(name, "WIL") ~ "Williamson_tmean",
-    startsWith(name, "SPR") ~ "Sprague_tmean "
+    startsWith(name, "SPR") ~ "Sprague_tmean"
   )) |>
   group_by(date, group) |>
   summarise(mean = mean(tmean)) |>
@@ -102,16 +99,12 @@ tmax_data_summary <- tmax_long_data |>
   group_by(date, group) |>
   summarise(mean = mean(tmax)) |>
   pivot_wider(names_from = "group", values_from ="mean")
-summary_data <- rbind(ppt_data_summary, tmean_data_summary, tmax_data_summary)
-write_csv(summary_data, "data/prism_data/prisma_summary.csv")
+summary_data <- ppt_data_summary |>
+  full_join(tmean_data_summary, by = c("date")) |>
+  full_join(tmax_data_summary, by = c("date")) |>
+  mutate(across(where(is.numeric), ~ round(.x, 2)))
+write_csv(summary_data, "data/prism_data/prism_summary.csv")
 
-# file <- "data-raw/20250201.zip"
-# unzip(file, exdir = "data-raw/Prism_Files")
-# ncin <- nc_open("data-raw/Prism_Files/PRISM_tmean_provisional_4kmD2_20250201_nc.nc")
-# lon <- ncvar_get(ncin,"lon")
-# lat <- ncvar_get(ncin, "lat")
-# precip <-ncvar_get(ncin, "Band1")
-# fillvalue <- ncatt_get(ncin, "Band1", "_FillValue")
 # nc_close(ncin)
 # precip[precip == fillvalue$value] <- NA
 # r <- raster(t(precip), xmn=min(lon), xmx=max(lon), ymn=min(lat), ymx=max(lat), crs=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs+ towgs84=0,0,0"))
@@ -119,10 +112,4 @@ write_csv(summary_data, "data/prism_data/prisma_summary.csv")
 # par(mar=c(1,1,1,1))
 # plot(r)i in 1:nrow(mat)
 # date_obj <- as.Date(date_str, format="%Y%m%d")
-# ppt_mm <- raster::extract(r, cbind(prism_coordinates[,"longitude"],prism_coordinates[,"latitude"]), method='simple')
-# ppt_inch <- ppt_mm/25.4
-# ppt_inch <- as.data.frame(as.numeric(ppt_inch))
-# colnames(ppt_inch) <- date_obj
-# prism_data <- as.data.frame(prism_locations)
-# prism_data <- cbind(prism_data, ppt_inch)
 
